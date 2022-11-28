f7325e2f3649df54bbd132ea629e6c2e6ffffc6a
Luk3-V <luke.vanhaezebrouck@gmail.com>
2022-11-27 22:31:57 -0800

    Add ssl api url

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -32,7 +32,7 @@ function App() {
   const [category, setCategory] = useState<String>("frameworks");
   const [theme, setTheme] = useState<boolean>(!localStorage.getItem("light") ? true : false);
   //const dataUrl = 'indeed/';
-  const dataUrl = 'http://3.18.174.51:5000/indeed/';
+  const dataUrl = 'https://indeedtechstacks.tk/indeed/';
 
   function toggleTheme() {
     document.body.classList.toggle('dark');

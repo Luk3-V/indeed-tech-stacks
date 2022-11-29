67b49f7c0b4086461409346d55a0f785fbb5b856
Luk3-V <luke.vanhaezebrouck@gmail.com>
2022-11-28 16:15:51 -0800

    Fix cron timezone

--- server/lib/cron.js
+++ server/lib/cron.js
@@ -2,8 +2,8 @@ const cron = require('node-cron');
 const IndeedCount = require('../models/IndeedCount');
 const IndeedScraperLite = require('../lib/indeedScraperLite');
 
-// 9 AM EET (Railway server) = 1 AM CST 
-cron.schedule('0 9 * * *', async () => { 
+// 6 AM UTC/GMT = 12 AM CST 
+cron.schedule('0 6 * * *', async () => { 
     console.log('RUNNING CRON: SCRAPE INDEED');
     let data = new IndeedCount( await IndeedScraperLite.getData({}) );
     data.save().then(() => console.log('CRON FINISHED!'));
